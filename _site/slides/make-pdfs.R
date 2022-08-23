9bb2a1659521ce36a5903073ccdd240baed269ec
Ryan Safner <ryansafner@gmail.com>
2022-08-22 21:24:49 -0400

    Update site formatting

---
+++ _site/slides/make-pdfs.R
@@ -0,0 +1,16 @@
+# knit Rmd slides to html
+rmarkdown::render("slides/1.1-slides.Rmd")
+
+# make-pdfs
+pagedown::chrome_print(input = "https://microf22.classes.ryansafner.com/slides/1.1-slides.html",
+                       output = "slides/1.1-slides.pdf",
+                       timeout = 300)
+
+# create png of first slide
+pagedown::chrome_print(input = "https://microf22.classes.ryansafner.com/slides/1.1-slides.html",
+                       output = "slides/1.1-slides.png",
+                       selector = "div.remark-slide",
+                       format = "png",
+                       timeout = 300)
+
+

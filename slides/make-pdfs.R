744e600f417239ba07211ec078716b56c840279f
Ryan Safner <ryansafner@gmail.com>
2022-10-06 11:11:25 -0400

    Update 2.6, term paper

--- slides/make-pdfs.R
+++ slides/make-pdfs.R
@@ -1,14 +1,14 @@
 # knit Rmd slides to html
-rmarkdown::render("slides/2.4-slides.Rmd")
+rmarkdown::render("slides/2.6-slides.Rmd")
 
 # make-pdfs
-pagedown::chrome_print(input = "https://thoughtf22.classes.ryansafner.com/slides/2.5-slides.html",
-                       output = "slides/2.5-slides.pdf",
+pagedown::chrome_print(input = "https://thoughtf22.classes.ryansafner.com/slides/2.6-slides.html",
+                       output = "slides/2.6-slides.pdf",
                        timeout = 300)
 
 # create png of first slide
-pagedown::chrome_print(input = "https://thoughtf22.classes.ryansafner.com/slides/2.5-slides.html",
-                       output = "slides/2.5-slides.png",
+pagedown::chrome_print(input = "https://thoughtf22.classes.ryansafner.com/slides/2.6-slides.html",
+                       output = "slides/2.6-slides.png",
                        selector = "div.remark-slide",
                        format = "png",
                        timeout = 300)
